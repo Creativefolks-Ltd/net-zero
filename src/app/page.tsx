e9013e19dbc83a11420f367438e817ddb9db97d8
joecf <joe@creativefolks.co.uk>
2023-11-03 11:34:38 +0000

    feat: homepage basic structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,91 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <header className="h-screen bg-black">
+        <div className="container h-full flex items-center">
+          <div className="w-1/2">
+            <h1 className="text-7xl mb-12 font-bold text-white">The Anthos Net Zero project</h1>
+            <h2 className="text-3xl font-bold text-white">Created to provide an ongoing service to help all family members</h2>
+          </div>
         </div>
-      </div>
+      </header>
+      <section className="section bg-brand-light">
+        <div className="container small">
+          <p className="uppercase text-2xl mb-2">The future of sustainability</p>
+          <h3 className="text-5xl w-2/3">Your personal carbon footprint calculator</h3>
+        </div>
+        <div className="container !max-w-6xl">
+          <div className="grid grid-cols-12 gap-24 py-16">
+            <div className="col-span-5 bg-brand"></div>
+            <div className="col-span-7">
+              <p>
+                <strong>Understand the shape and size of your carbon footprint.</strong>
+              </p>
+              <p>
+                <strong>Connect with an advisory service to discuss and explore opportunities for carbon reduction.</strong>
+              </p>
+              <p>Understand the shape and size of your carbon footprint.</p>
+              <p>Select from a choice of carbon offsets to compensate for emissions and become carbon neutral.</p>
+              <p>Repeat the process on an annual basis and track progress through time.</p>
+              <div className="mt-16">
+                <button className="bg-brand rounded-lg px-6 py-2 text-white font-bold text-lg">Calculate your footprint</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="section">
+        <div className="container">
+          <p className="uppercase text-2xl mb-2">How to complete the form</p>
+          <h3 className="text-5xl w-2/3">Form essentials</h3>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="py-16 w-5/6">
+            <p>The first step is to provide information through the tailored form, relating to your home, travel, shopping and assets. Information is encrypted and held on a secure server* and the form should take no more than 15 minutes to complete. On each page there are some sections that are required and others that are optional. The optional sections are for activities that we expect aren’t the biggest sources of emissions or which taken more time to complete. However, adding this information will give you a more accurate carbon footprint and more tailored recommendations.</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <p>To make the process quicker and easier, gather the following details before starting:</p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            <div className="grid grid-cols-3 gap-8">
+              <div className="p-8 flex flex-col text-center items-center">
+                <div className="border-2 border-brand rounded-full aspect-square w-32 h-32 mb-4"></div>
+                <p>Your residential energy consumption (electricity, natural gas, heating oil, any others)</p>
+              </div>
+              <div className="p-8 flex flex-col text-center items-center">
+                <div className="border-2 border-brand rounded-full aspect-square w-32 h-32 mb-4"></div>
+                <p>Your air travel use (number of flights/distance travelled)</p>
+              </div>
+              <div className="p-8 flex flex-col text-center items-center">
+                <div className="border-2 border-brand rounded-full aspect-square w-32 h-32 mb-4"></div>
+                <p>Your road travel use (vehicle information/ distance travelled)</p>
+              </div>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+            <p>You can save your responses if you are not able to complete the form, and return to it later. Once you have submitted the information, the net zero team will respond with initial results and suggested actions, and an invitation to arrange a consultation to discuss.</p>
+
+            <p>The tool has been built by Good Business and is based on the principles of the Greenhouse Gas Protocol. It has been developed specifically for Anthos and gives family members a tailored and personal service, making it stand out from the many carbon calculators available online.</p>
+
+            <div className="mt-16">
+              <button className="bg-brand rounded-lg px-6 py-2 text-white font-bold text-lg">Calculate your footprint</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section bg-grey-light">
+        <div className="container small">
+          <h3 className="text-center text-5xl mb-16">Protecting your data</h3>
+          <p className="text-center mb-16">All of your responses in this survey will be encrypted with TLS and only accessed by the Net-Zero team for the purposes of calculating your footprint and providing recommendations to reduce it. You can find more information in Good Business’ privacy policy here. The tool met the high data security requirements for penetration testing in March 2022.</p>
+
+          <div className="grid grid-cols-2 gap-16">
+            <div className="bg-black"></div>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo.</p>
+
+              <p>Sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor et accusam et dolores justo.</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,22 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
         brand: {
-          DEFAULT: "#81C14B",
+          light: "hsl(93, 49%, 95%)",
+          DEFAULT: "hsl(93, 49%, 53%)",
           secondary: "#84A98C",
-          error: "#FF0000",
-          grey: "#818181",
-          dark: "#31405A",
         },
+        error: "#FF0000",
+        grey: {
+          light: "hsl(0, 0%, 95%)",
+          DEFAULT: "hsl(0, 0%, 51%)",
+        },
+        dark: "#31405A",
       },
     },
   },
