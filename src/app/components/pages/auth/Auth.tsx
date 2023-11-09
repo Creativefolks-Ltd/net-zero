11b621c912664825581b5b151e47ce85f5412c36
joecf <joe@creativefolks.co.uk>
2023-11-09 16:24:52 +0000

    build: add eslint config and change root path on imports

--- src/app/components/pages/auth/Auth.tsx
+++ src/app/components/pages/auth/Auth.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import SignInForm from "./SignInForm";
-import Image from "next/image";
 import RegisterForm from "./RegisterForm";
 
 export default function Auth() {
@@ -10,22 +9,10 @@ export default function Auth() {
   return (
     <div className="md:bg-grey-lighter relative rounded-xl md:pt-12 md:pb-32 px-8 md:px-24">
       <div className="flex justify-center md:justify-start mb-3">
-        <button
-          className={`p-4 text-xl md:text-2xl relative text-grey-dark duration-300 transition-all font-bold   auth-tab  cursor-pointer ${
-            signInTab && "auth-tab-selected"
-          }`}
-          onKeyDown={() => setSignInTab(true)}
-          onClick={() => setSignInTab(true)}
-        >
+        <button className={`p-4 text-xl md:text-2xl relative text-grey-dark duration-300 transition-all font-bold   auth-tab  cursor-pointer ${signInTab && "auth-tab-selected"}`} onKeyDown={() => setSignInTab(true)} onClick={() => setSignInTab(true)}>
           Sign in
         </button>
-        <button
-          className={`p-4 text-xl md:text-2xl relative text-grey-dark duration-300 transition-all font-bold   auth-tab cursor-pointer ${
-            !signInTab && "auth-tab-selected"
-          }`}
-          onKeyDown={() => setSignInTab(false)}
-          onClick={() => setSignInTab(false)}
-        >
+        <button className={`p-4 text-xl md:text-2xl relative text-grey-dark duration-300 transition-all font-bold   auth-tab cursor-pointer ${!signInTab && "auth-tab-selected"}`} onKeyDown={() => setSignInTab(false)} onClick={() => setSignInTab(false)}>
           Register
         </button>
       </div>
--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import PersonalInformation from "../components/pages/my-account/PersonalInformation";
-import Header from "../components/core/Header";
-import SavedForms from "../components/pages/my-account/SavedForms";
+import PersonalInformation from "@/components/pages/my-account/PersonalInformation";
+import Header from "@/components/core/Header";
+import SavedForms from "@/components/pages/my-account/SavedForms";
 
 export default function MyAccount() {
   return (
     <>
       <Header />
       <main className="container px-12 pt-16 pb-36">
-        <h1 className="font-bold text-brand-dark text-4xl md:text-5xl mb-8 md:mb-16">
-          My account{" "}
-        </h1>
+        <h1 className="font-bold text-brand-dark text-4xl md:text-5xl mb-8 md:mb-16">My account </h1>
         <div className="flex md:flex-row flex-col gap-12">
           <PersonalInformation />
           <SavedForms />
